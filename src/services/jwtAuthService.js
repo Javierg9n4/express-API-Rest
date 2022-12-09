bf725e5d11fe4d0c8b7d4591dbed5bd1a9436de0
Javierg9n4 <javierg9n4@gmail.com>
2022-12-09 16:54:48 +0100

    jwt authentication added (not working yet)

---
+++ src/services/jwtAuthService.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+const path = require("path");
+const env = require("dotenv").config({
+  path: path.resolve(__dirname, "../..", ".env"),
+}).parsed;
+
+const createJWT = () => {
+  console.log("hola")
+  const JWT_SECRET = `"${env.JWT_SECRET}"`;
+
+  const token = jwt.sign({ data: "jwt value" }, JWT_SECRET, {
+    expiresIn: "24h",
+  });
+
+  return token;
+};
+
+module.exports = {
+  createJWT,
+};
